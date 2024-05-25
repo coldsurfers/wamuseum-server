30059918b56b39aacb1974cc150cbca99ba3d12a
yungblud <killi8n@gmail.com>
2024-05-25 13:00:02 +0900

    feat: implemented concert model

--- src/models/Concert.ts
+++ src/models/Concert.ts
@@ -1,82 +1,15 @@
+import { type Concert as PrismaConcert } from '@coldsurfers/prisma-schema'
 import { prisma } from '..'
-import ConcertPoster from './ConcertPoster'
-import ConcertTicket from './ConcertTicket'
 
-class Concert {
-  public id?: string
+class ConcertModel {
+  public prismaModel: PrismaConcert
 
-  public concertCategoryId: number
-
-  public artist?: string
-
-  public title: string
-
-  public location: string
-
-  public date?: Date
-
-  public posters?: Pick<
-    ConcertPoster,
-    'concertId' | 'id' | 'createdAt' | 'imageURL'
-  >[]
-
-  public tickets?: Pick<
-    ConcertTicket,
-    | 'concertId'
-    | 'id'
-    | 'createdAt'
-    | 'ticketPrices'
-    | 'openDate'
-    | 'seller'
-    | 'sellingURL'
-  >[]
-
-  public createdAt?: Date
-
-  public updatedAt?: Date
-
-  public html?: string
-
-  constructor(
-    params: Required<
-      Pick<
-        Concert,
-        'concertCategoryId' | 'artist' | 'title' | 'date' | 'location'
-      >
-    > &
-      Pick<
-        Concert,
-        'id' | 'posters' | 'tickets' | 'createdAt' | 'updatedAt' | 'html'
-      >
-  ) {
-    const {
-      id,
-      concertCategoryId,
-      artist,
-      title,
-      date,
-      posters,
-      tickets,
-      createdAt,
-      updatedAt,
-      location,
-      html,
-    } = params
-    this.id = id
-    this.concertCategoryId = concertCategoryId
-    this.artist = artist
-    this.title = title
-    this.location = location
-    this.date = date
-    this.posters = posters
-    this.tickets = tickets
-    this.createdAt = createdAt
-    this.updatedAt = updatedAt
-    this.html = html
+  constructor(concert: PrismaConcert) {
+    this.prismaModel = concert
   }
 
-  public static async find(id: string) {
-    const concert = await prisma.adminConcert.findUnique({
+  public static async findById(id: string) {
+    const concert = await prisma.concert.findUnique({
       where: {
         id,
       },
@@ -90,10 +23,10 @@ class Concert {
         concertCategory: true,
       },
     })
-    return concert
+    return concert ? new ConcertModel(concert) : null
   }
 
-  public static async list({
+  public static async paginate({
     page,
     limit,
     orderBy,
@@ -104,7 +37,7 @@ class Concert {
       createdAt?: 'asc' | 'desc'
     }
   }) {
-    const list = await prisma.adminConcert.findMany({
+    const list = await prisma.concert.findMany({
       include: {
         posters: true,
         tickets: {
@@ -118,156 +51,16 @@ class Concert {
       take: limit,
       orderBy,
     })
-    return list
+    return list.map((listItem) => new ConcertModel(listItem))
   }
 
   public static async count() {
-    const count = await prisma.adminConcert.count()
+    const count = await prisma.concert.count()
     return count
   }
 
-  public async create() {
-    const {
-      artist,
-      date,
-      title,
-      tickets,
-      posters,
-      concertCategoryId,
-      location,
-    } = this
-    const concert = await prisma.adminConcert.create({
-      data: {
-        concertCategory: {
-          connect: {
-            id: concertCategoryId,
-          },
-        },
-        artist,
-        location,
-        date: date ? new Date(date) : undefined,
-        title,
-        tickets: tickets
-          ? {
-              create: tickets.map((ticket) => ({
-                ...ticket,
-                ticketPrices: ticket.ticketPrices
-                  ? {
-                      createMany: {
-                        data: ticket.ticketPrices,
-                      },
-                    }
-                  : undefined,
-              })),
-            }
-          : undefined,
-        posters: posters
-          ? {
-              createMany: {
-                data: posters.map(({ imageURL }) => ({
-                  imageURL,
-                })),
-              },
-            }
-          : undefined,
-      },
-      select: {
-        id: true,
-        artist: true,
-        date: true,
-        title: true,
-        location: true,
-        tickets: {
-          include: {
-            ticketPrices: true,
-          },
-        },
-        posters: true,
-        createdAt: true,
-        concertCategory: true,
-      },
-    })
-    return concert
-  }
-
-  public async update() {
-    const {
-      id,
-      artist,
-      title,
-      date,
-      posters,
-      tickets,
-      concertCategoryId,
-      location,
-    } = this
-    const updated = await prisma.adminConcert.update({
-      data: {
-        concertCategory: {
-          connect: {
-            id: concertCategoryId,
-          },
-        },
-        artist,
-        title,
-        location,
-        date,
-        posters: posters
-          ? {
-              deleteMany: {
-                concertId: id,
-              },
-              createMany: {
-                data: posters.map((poster) => ({
-                  imageURL: poster.imageURL,
-                })),
-              },
-            }
-          : undefined,
-        tickets: tickets
-          ? {
-              deleteMany: {
-                concertId: id,
-              },
-              create: tickets.map((ticket) => ({
-                ...ticket,
-                ticketPrices: ticket.ticketPrices
-                  ? {
-                      createMany: {
-                        data: ticket.ticketPrices,
-                      },
-                    }
-                  : undefined,
-              })),
-            }
-          : undefined,
-        updatedAt: new Date(),
-      },
-      where: {
-        id,
-      },
-      select: {
-        id: true,
-        artist: true,
-        date: true,
-        title: true,
-        location: true,
-        tickets: {
-          include: {
-            ticketPrices: true,
-          },
-        },
-        posters: true,
-        createdAt: true,
-        updatedAt: true,
-        concertCategory: true,
-      },
-    })
-    return updated
-  }
-
-  public static async remove(id: string) {
-    const deleted = await prisma.adminConcert.delete({
+  public static async delete(id: string) {
+    const deleted = await prisma.concert.delete({
       where: {
         id,
       },
@@ -279,4 +72,4 @@ class Concert {
   }
 }
 
-export default Concert
+export default ConcertModel
