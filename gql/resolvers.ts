428b738d5cb9fb66a22e1663e4242f7542d2e381
yungblud <killi8n@gmail.com>
2024-08-31 23:59:41 +0900

    feat: concert mutations

--- gql/resolvers.ts
+++ gql/resolvers.ts
@@ -9,7 +9,6 @@ import {
   ConcertCategoryService,
   UserService,
   StaffService,
-  ConcertService,
 } from '../src/services'
 import { Resolvers } from './resolvers-types'
 import { sendEmail } from '../src/utils/mailer'
@@ -77,6 +76,7 @@ const resolvers: Resolvers = {
   Mutation: {
     ...userResolvers.Mutation,
     ...authResolvers.Mutation,
+    ...concertResolvers.Mutation,
     createEmailAuthRequest: async (parent, args) => {
       const { email } = args.input
       const createdEmailAuthRequest = await EmailAuthRequestService.create({
@@ -162,130 +162,6 @@ const resolvers: Resolvers = {
       const { title } = args.input
       const concertCategory = await ConcertCategoryService.create({ title })
       return concertCategory
-    },
-    createConcert: async (parent, args, ctx) => {
-      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
-      if (!user) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const { id: userId } = user
-      const staff = await StaffService.getStaffByUserId(userId)
-      if (!staff) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const {
-        concertCategoryId,
-        artist,
-        date,
-        title,
-        tickets,
-        posterURLs,
-        location,
-      } = args.input
-      const concert = await ConcertService.create({
-        concertCategoryId,
-        artist,
-        location,
-        date: new Date(date),
-        title,
-        tickets: tickets.map((ticket) => ({
-          ...ticket,
-          openDate: new Date(ticket.openDate),
-        })),
-        posters: posterURLs.map((url) => ({
-          imageURL: url,
-        })),
-      })
-
-      return concert
-    },
-    updateConcert: async (parent, args, ctx) => {
-      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
-      if (!user) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const { id: userId } = user
-      const staff = await StaffService.getStaffByUserId(userId)
-      if (!staff) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const {
-        id,
-        artist,
-        date,
-        title,
-        posterURLs,
-        tickets,
-        concertCategoryId,
-        location,
-      } = args.input
-      const updated = await ConcertService.updateById(id, {
-        artist,
-        date: new Date(date),
-        title,
-        location,
-        posters: posterURLs
-          ? posterURLs.map((url) => ({
-              imageURL: url,
-            }))
-          : undefined,
-        tickets: tickets
-          ? tickets.map((ticket) => ({
-              ...ticket,
-              openDate: new Date(ticket.openDate),
-            }))
-          : undefined,
-        concertCategoryId,
-      })
-
-      return updated
-    },
-    removeConcert: async (parent, args, ctx) => {
-      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
-      if (!user) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const { id: userId } = user
-      const staff = await StaffService.getStaffByUserId(userId)
-      if (!staff) {
-        throw new GraphQLError('권한이 없습니다', {
-          extensions: {
-            code: 401,
-          },
-        })
-      }
-      const existing = await ConcertService.getConcertById(args.input.id)
-      if (!existing)
-        return {
-          __typename: 'HttpError',
-          code: 404,
-          message: '콘서트가 존재하지 않습니다.',
-        }
-      const removed = await ConcertService.deleteById(args.input.id)
-      return {
-        id: removed.id,
-        __typename: 'RemovedConcert',
-      }
     },
     updateConcertTicket: async (parent, args, ctx) => {
       const user = await UserService.getUserByAccessToken(ctx.token ?? '')
--- src/resolvers/concertResolvers.ts
+++ src/resolvers/concertResolvers.ts
@@ -74,6 +74,132 @@ const concertResolvers: Resolvers = {
       }
     },
   },
+  Mutation: {
+    createConcert: async (parent, args, ctx) => {
+      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
+      if (!user) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const { id: userId } = user
+      const staff = await StaffService.getStaffByUserId(userId)
+      if (!staff) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const {
+        concertCategoryId,
+        artist,
+        date,
+        title,
+        tickets,
+        posterURLs,
+        location,
+      } = args.input
+      const concert = await ConcertService.create({
+        concertCategoryId,
+        artist,
+        location,
+        date: new Date(date),
+        title,
+        tickets: tickets.map((ticket) => ({
+          ...ticket,
+          openDate: new Date(ticket.openDate),
+        })),
+        posters: posterURLs.map((url) => ({
+          imageURL: url,
+        })),
+      })
+
+      return concert
+    },
+    updateConcert: async (parent, args, ctx) => {
+      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
+      if (!user) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const { id: userId } = user
+      const staff = await StaffService.getStaffByUserId(userId)
+      if (!staff) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const {
+        id,
+        artist,
+        date,
+        title,
+        posterURLs,
+        tickets,
+        concertCategoryId,
+        location,
+      } = args.input
+      const updated = await ConcertService.updateById(id, {
+        artist,
+        date: new Date(date),
+        title,
+        location,
+        posters: posterURLs
+          ? posterURLs.map((url) => ({
+              imageURL: url,
+            }))
+          : undefined,
+        tickets: tickets
+          ? tickets.map((ticket) => ({
+              ...ticket,
+              openDate: new Date(ticket.openDate),
+            }))
+          : undefined,
+        concertCategoryId,
+      })
+
+      return updated
+    },
+    removeConcert: async (parent, args, ctx) => {
+      const user = await UserService.getUserByAccessToken(ctx.token ?? '')
+      if (!user) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const { id: userId } = user
+      const staff = await StaffService.getStaffByUserId(userId)
+      if (!staff) {
+        throw new GraphQLError('권한이 없습니다', {
+          extensions: {
+            code: 401,
+          },
+        })
+      }
+      const existing = await ConcertService.getConcertById(args.input.id)
+      if (!existing)
+        return {
+          __typename: 'HttpError',
+          code: 404,
+          message: '콘서트가 존재하지 않습니다.',
+        }
+      const removed = await ConcertService.deleteById(args.input.id)
+      return {
+        id: removed.id,
+        __typename: 'RemovedConcert',
+      }
+    },
+  },
 }
 
 export default concertResolvers
