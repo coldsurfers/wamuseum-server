03b281a0c95e926c1ac68c4b974c3d3a95cf0924
yungblud <killi8n@gmail.com>
2024-09-18 22:08:54 +0900

    feat: implemented create, connect with concert functions in VenueDTO

--- src/dtos/VenueDTO.ts
+++ src/dtos/VenueDTO.ts
@@ -1,4 +1,5 @@
 import { Venue } from '@prisma/client'
+import { prisma } from '..'
 
 type VenueDTOProps = Partial<Venue>
 
@@ -7,5 +8,36 @@ export default class VenueDTO {
 
   constructor(props: VenueDTOProps) {
     this.props = props
+  }
+
+  async create() {
+    const { lat, lng, name, geohash } = this.props
+    if (!lat || !lng || !name || !geohash) {
+      throw Error('invalid props')
+    }
+    const data = await prisma.venue.create({
+      data: {
+        lat,
+        lng,
+        name,
+        geohash,
+      },
+    })
+    return new VenueDTO(data)
+  }
+
+  async connect(concertId: string) {
+    if (!this.props.id) {
+      throw Error('invalid id.')
+    }
+    const data = await prisma.concertsOnVenues.create({
+      data: {
+        concertId,
+        venueId: this.props.id,
+      },
+    })
+    return new VenueDTO({
+      id: data.venueId,
+    })
   }
 }
